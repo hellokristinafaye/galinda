a32e8645ce7ebaf67549ed0d4eee04441e45ee4a
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-23 16:04:41 -0700

    Dummy data started

---
+++ src/components/Accordian/data.js
@@ -0,0 +1,24 @@
+// dummy data 
+
+const data = [
+    {
+        id: '1',
+        question: "What are accordion components?",
+        answer: "Accordions coponentns are user interface elements used for organized is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s "
+    },
+    {
+        id: '1',
+        question: "What are accordion components?",
+        answer: "Accordions coponentns are user interface elements used for organized is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s "
+    },
+    {
+        id: '1',
+        question: "What are accordion components?",
+        answer: "Accordions coponentns are user interface elements used for organized is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s "
+    },
+    {
+        id: '1',
+        question: "What are accordion components?",
+        answer: "Accordions coponentns are user interface elements used for organized is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s "
+    },
+]
