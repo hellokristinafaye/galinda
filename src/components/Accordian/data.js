69f27760fb697d542d7f49d11f0172a39042b440
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-23 16:06:34 -0700

    Dummy data simulated. Lorem shortcut not working tho?

--- src/components/Accordian/data.js
+++ src/components/Accordian/data.js
@@ -7,18 +7,18 @@ const data = [
         answer: "Accordions coponentns are user interface elements used for organized is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s "
     },
     {
-        id: '1',
-        question: "What are accordion components?",
-        answer: "Accordions coponentns are user interface elements used for organized is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s "
+        id: '2',
+        question: "What making it look like readable English?",
+        answer: "interface elements used for organized is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s "
     },
     {
-        id: '1',
-        question: "What are accordion components?",
-        answer: "Accordions coponentns are user interface elements used for organized is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s "
+        id: '3',
+        question: "at Hampden-Sydney College in Virginia?",
+        answer: "coponentns are user interface elements used for organized is simply dummy text of the printing and psum has been the industry's standard dummy text ever since the 1500s "
     },
     {
-        id: '1',
-        question: "What are accordion components?",
-        answer: "Accordions coponentns are user interface elements used for organized is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s "
+        id: '4',
+        question: "versions from the 1914 translation by H. Rackham?",
+        answer: "printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s "
     },
 ]
