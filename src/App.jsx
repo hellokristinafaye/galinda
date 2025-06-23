9062cf7fe2df73b156cbe0ba2be96222b1b51e19
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-23 15:53:12 -0700

    Accordian exercise START

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Accordian from './components/Accordian/Accordian'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+    <Accordian/>
     </>
   )
 }
