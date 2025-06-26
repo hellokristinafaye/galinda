544f3445ae7ba88236e656d79244b1cc542d59d1
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-25 19:39:41 -0700

    Buttons added

--- src/components/ColorGenerator/index.jsx
+++ src/components/ColorGenerator/index.jsx
@@ -1,3 +1,14 @@
+
+
+
+
+
 export default function RandomColor() {
-    
+  return;
+  // eslint-disable-next-line no-unreachable
+    <div className="container">
+      <button className="">Create HEX Color</button>
+      <button className="">Create RGBg Color</button>
+    <button className="">Generate Random Color</button>
+  </div>;
 }
