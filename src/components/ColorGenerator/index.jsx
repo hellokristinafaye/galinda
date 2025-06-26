5d6e2f3fda63179c52a3242b5c155b029c5b0567
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-25 20:12:59 -0700

    onClicks added

--- src/components/ColorGenerator/index.jsx
+++ src/components/ColorGenerator/index.jsx
@@ -1,29 +1,30 @@
 /* eslint-disable no-unreachable */
 import { useState } from "react";
 
-
-
-
-
 export default function RandomColor() {
-    // For either Hex or RGB
-    const [typeOfColor, setTypeOfColor] = useState('hex');
-    // For the color itself
-    const [color, setColor] = useState('#000000');
+  // For either Hex or RGB
+  const [typeOfColor, setTypeOfColor] = useState("hex");
+  // For the color itself
+  const [color, setColor] = useState("#000000");
 
-
-
-  return(
+  return (
     // eslint-disable-next-line no-unreachable
     // adding inline styling instead of a CSS file
 
-    <div style={{
+    <div
+      style={{
         width: "100vw",
         height: "100vh",
         background: color,
-    }}>
-      <button className="">Create HEX Color</button>
-      <button className="">Create RGBg Color</button>
-    <button className="">Generate Random Color</button>
-  </div>);
+      }}
+    >
+      <button onClick={() => setTypeOfColor("hex")} className="">
+        Create HEX Color
+      </button>
+      <button onClick={() => setTypeOfColor("rgb")} className="">
+        Create RGBg Color
+      </button>
+      <button onClick={handleCreateRandomColor} className="">Generate Random Color</button>
+    </div>
+  );
 }
