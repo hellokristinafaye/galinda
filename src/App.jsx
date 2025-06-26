df4ce5cc89afe020b2967ac226ed68cd8e9a8b29
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-25 20:56:26 -0700

    commented out accordion

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-// import Accordian from './components/Accordian/Accordian'
+import Accordian from './components/Accordian/Accordian'
 import RandomColor from './components/ColorGenerator'
 
 function App() {
