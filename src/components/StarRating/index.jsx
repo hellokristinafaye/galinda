d009163601f1d7d5c94a2c604574682b4d8fe9ce
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-26 21:08:17 -0700

    state vars added

--- src/components/StarRating/index.jsx
+++ src/components/StarRating/index.jsx
@@ -1,7 +1,11 @@
 // Trial 2 START
+import { useState } from 'react';
 import {FaStar} from 'react-icons/fa'
 
-export default function StarRating({noOfStars = 5}) {
+export default function StarRating({ noOfStars = 5 }) {
+    
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
   
   
     return (
