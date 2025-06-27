5be65218bc2a5d649a6f77a7af6be430789b934c
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-27 13:22:30 -0700

    Starting Trial 3, will move to new branch next

--- src/components/StarRating/index.jsx
+++ src/components/StarRating/index.jsx
@@ -1,47 +1,57 @@
+// Trial 3 START
+
+
+
+
+
+
+// Trial 3 END
+
+
 // Trial 2 START
-import { useState } from "react";
-import { FaStar } from "react-icons/fa";
-import "./style.css";
+// import { useState } from "react";
+// import { FaStar } from "react-icons/fa";
+// import "./style.css";
 
-export default function StarRating({ noOfStars = 5 }) {
-  // state vars
-  const [rating, setRating] = useState(0);
-  const [hover, setHover] = useState(0);
+// export default function StarRating({ noOfStars = 5 }) {
+//   // state vars
+//   const [rating, setRating] = useState(0);
+//   const [hover, setHover] = useState(0);
 
-  // custom methods
-  const handleClick = (getCurrentIndex) => {
-      console.log(getCurrentIndex, "click!");
-      setRating(getCurrentIndex);
-      console.log(rating, "click rating")
-  };
-  const handleMouseEnter = (getCurrentIndex) => {
-      console.log(getCurrentIndex, "over!");
-      setHover(getCurrentIndex);
-      console.log(hover, "hover!")
-  };
-  const handleMouseLeave = () => {
-      setHover(rating);
-      console.log(rating, "gone!");
-  };
+//   // custom methods
+//   const handleClick = (getCurrentIndex) => {
+//       console.log(getCurrentIndex, "click!");
+//       setRating(getCurrentIndex);
+//       console.log(rating, "click rating")
+//   };
+//   const handleMouseEnter = (getCurrentIndex) => {
+//       console.log(getCurrentIndex, "over!");
+//       setHover(getCurrentIndex);
+//       console.log(hover, "hover!")
+//   };
+//   const handleMouseLeave = () => {
+//       setHover(rating);
+//       console.log(rating, "gone!");
+//   };
 
-  return (
-    <div className="">
-      {[...Array(noOfStars)].map((_, index) => {
-        index += 1;
-        return (
-          <FaStar
-            key={index}
-            onClick={() => handleClick(index)}
-            onMouseOver={() => handleMouseEnter(index)}
-            onMouseLeave={() => handleMouseLeave(index)}
-            size={40}
-            className={index <= (hover || rating) ? "active" : "inactive"}
-          />
-        );
-      })}
-    </div>
-  );
-}
+//   return (
+//     <div className="">
+//       {[...Array(noOfStars)].map((_, index) => {
+//         index += 1;
+//         return (
+//           <FaStar
+//             key={index}
+//             onClick={() => handleClick(index)}
+//             onMouseOver={() => handleMouseEnter(index)}
+//             onMouseLeave={() => handleMouseLeave(index)}
+//             size={40}
+//             className={index <= (hover || rating) ? "active" : "inactive"}
+//           />
+//         );
+//       })}
+//     </div>
+//   );
+// }
 
 // Trial 2 END
 
