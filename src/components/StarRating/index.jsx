a218c7e48de86683827a8c6d603fdafd60f1129a
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-26 19:47:59 -0700

    Icon imported, map func started

--- src/components/StarRating/index.jsx
+++ src/components/StarRating/index.jsx
@@ -1,11 +1,20 @@
+import {FaStar} from 'react-icons/fa'
+
 
 export default function StarRating(numOfStars = 5){
 
     return (
         <div className="">Star Rating
+{
+            [...Array(numOfStars)].map((_, index) => {
 
-            [...Array(numOfStars)].map()
-        
+                return <FaStar
+                    key={index}
+                    
+                
+            />
+            })
+        }
         </div>
     )
 }
