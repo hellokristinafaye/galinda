21edaedc5f712b055f7813b87d8c8d229fe1020f
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-26 20:13:26 -0700

    conditional className rendering based on the hover or rating index

--- src/components/StarRating/index.jsx
+++ src/components/StarRating/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
-export default function StarRating({noOfStars = 5}) {
+export default function StarRating({ noOfStars = 5 }) {
   // State vars
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
@@ -32,6 +32,8 @@ export default function StarRating({noOfStars = 5}) {
             onMouseLeave={() => handleMouseLeave(index)}
             onMouseOver={() => handleMouseEnter(index)}
             size={40}
+            // is the index lessthan or equal to the hover or rating indexes?
+            className={index <= (hover || rating) ? "active" : "inactive"}
           />
         );
       })}
