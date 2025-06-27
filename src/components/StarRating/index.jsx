20a630e3ac78a15cb65e0895e4cc2dbaa0ca52c1
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-26 21:15:05 -0700

    Conditional className added

--- src/components/StarRating/index.jsx
+++ src/components/StarRating/index.jsx
@@ -31,7 +31,8 @@ export default function StarRating({ noOfStars = 5 }) {
                     onClick={() => handleClick(index)}
                     onMouseOver={() => handleMouseEnter(index)}
                     onMouseLeave={() => handleMouseLeave(index)}
-                    size={40}
+                        size={40}
+                        className={index <= (hover || rating)? 'active': 'inactive'}
                   />
                 );
         })}
