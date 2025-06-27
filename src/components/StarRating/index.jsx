5b9ef30b54d729d40e07a85186f5a6339b108318
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-26 20:04:52 -0700

    missing the curly braces from the component function args

--- src/components/StarRating/index.jsx
+++ src/components/StarRating/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
-export default function StarRating(noOfStars = 5) {
+export default function StarRating({noOfStars = 5}) {
   // State vars
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
@@ -22,14 +22,14 @@ export default function StarRating(noOfStars = 5) {
   return (
     <div className="">
       {[...Array(noOfStars)].map((_, index) => {
-        index += 1;
+        // index += 1;
 
         return (
           <FaStar
             key={index}
-            onClick={() => handleClick(index)}
-            onMouseMove={() => handleMouseEnter(index)}
-            onMouseLeave={() => handleMouseLeave(index)}
+            // onClick={() => handleClick(index)}
+            // onMouseMove={() => handleMouseEnter(index)}
+            // onMouseLeave={() => handleMouseLeave(index)}
             size={40}
           />
         );
