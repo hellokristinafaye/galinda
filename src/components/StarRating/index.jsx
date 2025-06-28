395c8ee22682bae5e9f90c166fa43bfca4c7974d
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-28 14:57:21 -0700

    index number fixed

--- src/components/StarRating/index.jsx
+++ src/components/StarRating/index.jsx
@@ -25,6 +25,8 @@ export default function StarRating({ noOfStars = 5 }) {
   return (
     <div className="">
       {[...Array(noOfStars)].map((_, index) => {
+        index += 1
+
         return (
           <FaStar
             key={index}
