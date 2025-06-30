808b25fbaa9bcc029ad6fda9795147a4a8144ec6
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-30 16:53:46 -0700

    Custom Hook start

--- src/components/LocalStorage/useLocalStorage.jsx
+++ src/components/LocalStorage/useLocalStorage.jsx
@@ -1,7 +1,22 @@
 // Trial 2 START
 
+import { useState } from "react";
 
 
+export default function useLocalStorage(key, defaultValue) {
+    
+    const [value, setValue] = useState(() => {
+        
+        let currentValue;
+
+        try {
+                    
+        } catch (error) {
+            
+        }
+
+    })
+}
 
 
 
