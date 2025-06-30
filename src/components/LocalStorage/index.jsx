eb6fc3ab94a03993d7316fd6a35330e8a1bfb4fe
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-30 16:48:40 -0700

    Commenting old code for next trial

--- src/components/LocalStorage/index.jsx
+++ src/components/LocalStorage/index.jsx
@@ -1,21 +1,38 @@
-import useLocalStorage from "./useLocalStorage";
-import './theme.css'
 
-export default function LightDarkMode() {
-  const [theme, setTheme] = useLocalStorage("theme", "dark");
 
-  function handleToggleTheme() {
-    setTheme(theme === "light" ? "dark" : "light");
-  }
 
-  return (
-    <div className="light-dark-mode" data-theme={theme}>
-      <div className="container">
-        <p className="">Hello World!</p>
-        <button onClick={handleToggleTheme} className="">
-          Change Theme
-        </button>
-      </div>
-    </div>
-  );
-}
+
+
+
+
+
+
+
+
+
+
+// Trial 1 START
+// import useLocalStorage from "./useLocalStorage";
+// import './theme.css'
+
+// export default function LightDarkMode() {
+//   const [theme, setTheme] = useLocalStorage("theme", "dark");
+
+//   function handleToggleTheme() {
+//     setTheme(theme === "light" ? "dark" : "light");
+//   }
+
+//   return (
+//     <div className="light-dark-mode" data-theme={theme}>
+//       <div className="container">
+//         <p className="">Hello World!</p>
+//         <button onClick={handleToggleTheme} className="">
+//           Change Theme
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
+
+// Trial 1 END
