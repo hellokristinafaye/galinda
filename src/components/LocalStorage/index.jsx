866040ce770661186d7d8fa7b1675aa1d2e3569b
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-30 15:09:21 -0700

    Local Storage/Light-Dark theme START

---
+++ src/components/LocalStorage/index.jsx
@@ -0,0 +1,10 @@
+export default function LightDarkMode() {
+  return (
+    <div className="light-dark-mode">
+      <div className="container">
+        <p className="">Hello World!</p>
+        <button className="">Change Theme</button>
+      </div>
+    </div>
+  );
+}
