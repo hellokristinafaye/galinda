4fdb333c458e93ce22b876f586d67c6b1919303a
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-30 15:18:24 -0700

    state var using custom hook

--- src/components/LocalStorage/index.jsx
+++ src/components/LocalStorage/index.jsx
@@ -1,4 +1,9 @@
+import useLocalStorage from "./useLocalStorage";
+
 export default function LightDarkMode() {
+
+    const [theme, setTheme] = useLocalStorage('theme', 'dark');
+
   return (
     <div className="light-dark-mode">
       <div className="container">
