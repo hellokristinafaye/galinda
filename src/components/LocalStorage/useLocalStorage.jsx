257ac1b7fdfe6187d043c34d3452f2724c8de3bb
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-30 15:14:17 -0700

    The GET portion of local storage

--- src/components/LocalStorage/useLocalStorage.jsx
+++ src/components/LocalStorage/useLocalStorage.jsx
@@ -1,5 +1,19 @@
 
 
 export default function useLocalStorage(key, defaultValue) {
-    
+
+    const [value, setValue] = useState(() => {
+        let currentValue;
+
+        try {
+
+            currentValue = JSON.parse(localStorage.getItem(key) || String(defaultValue))
+
+        } catch (error) {
+            console.log(error);
+            currentValue = defaultValue
+        }
+
+        return currentValue;
+    })
 }
