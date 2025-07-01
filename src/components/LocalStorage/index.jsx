77e88453bc355c5190214dfa0046c62bd57a7fc9
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-30 17:39:12 -0700

    Found syntax error: data-theme attr value needs to be in curly braces

--- src/components/LocalStorage/index.jsx
+++ src/components/LocalStorage/index.jsx
@@ -13,7 +13,7 @@ export default function LightDarkMode() {
     console.log(theme);
     
     return (
-      <div className="light-dark-mode" data-theme="theme">
+      <div className="light-dark-mode" data-theme={theme}>
         <div className="container">
           <p className="">Hello World!</p>
           <button onClick={handleToggleTheme} className="">
