55ed59e310d13449caf7dd7b866a50de84851673
Kristina Faye <hellokristinafaye@gmail.com>
2025-07-01 21:42:29 -0700

    try/catch done

--- src/components/LocalStorage/useLocalStorage.jsx
+++ src/components/LocalStorage/useLocalStorage.jsx
@@ -1,7 +1,24 @@
 // Trial 7 START
 
+import { useState } from "react";
 
 
+export default function useLocalStorage(key, defaultValue) {
+  
+  const [value, setValue] = useState(() => {
+    let currentValue;
+
+    try {
+      currentValue = JSON.parse(localStorage.getItem(key) || String(defaultValue))
+    } catch (error) {
+      console.log(error)
+      currentValue = defaultValue
+    }
+    return currentValue;
+  })
+
+  
+}
 
 
 
